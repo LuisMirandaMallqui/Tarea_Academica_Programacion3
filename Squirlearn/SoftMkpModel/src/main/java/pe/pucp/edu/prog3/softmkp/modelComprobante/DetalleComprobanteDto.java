b2d089d0af87eb72df25d6602b1b3372dfe58031
Gabo1422 <a20222746@pucp.edu.pe>
2025-09-30 10:48:37 -0500

    Actualización
    Cambio de acuerdo a las correciones del JP

---
+++ Squirlearn/SoftMkpModel/src/main/java/pe/pucp/edu/prog3/softmkp/modelComprobante/DetalleComprobanteDto.java
@@ -0,0 +1,108 @@
+package pe.pucp.edu.prog3.softmkp.modelComprobante;
+
+import java.util.Date;
+import pe.pucp.edu.prog3.softmkp.modelAlquiler.AlquilerDto;
+import pe.pucp.edu.prog3.softmkp.modelItem.ItemDto;
+
+public class DetalleComprobanteDto {
+    
+    private Integer detalleComId;
+    private String descripcion;
+    private Double precio;
+    private ItemDto item;
+    private AlquilerDto alquiler;
+    private ComprobanteDto comprobante;
+    private Date fechaModificacion;
+    private Integer usuarioModificacion;
+    
+    // Constructor vacío
+    public DetalleComprobanteDto() {
+        this.detalleComId = null;
+        this.descripcion = null;
+        this.precio = null;
+        this.item = null;
+        this.alquiler = null;
+        this.comprobante = null;
+        this.fechaModificacion = null;
+        this.usuarioModificacion = null;
+    }
+
+    // Constructor con parámetros
+    public DetalleComprobanteDto(Integer detalleComId, String descripcion, Double precio,
+                                 ItemDto item, AlquilerDto alquiler, ComprobanteDto comprobante,
+                                 Date fechaModificacion, Integer usuarioModificacion) {
+        this.detalleComId = detalleComId;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.item = item;
+        this.alquiler = alquiler;
+        this.comprobante = comprobante;
+        this.fechaModificacion = fechaModificacion;
+        this.usuarioModificacion = usuarioModificacion;
+    }
+
+    // Getters y Setters
+    public Integer getDetalleComId() {
+        return detalleComId;
+    }
+
+    public void setDetalleComId(Integer detalleComId) {
+        this.detalleComId = detalleComId;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public ItemDto getItem() {
+        return item;
+    }
+
+    public void setItem(ItemDto item) {
+        this.item = item;
+    }
+
+    public AlquilerDto getAlquiler() {
+        return alquiler;
+    }
+
+    public void setAlquiler(AlquilerDto alquiler) {
+        this.alquiler = alquiler;
+    }
+
+    public ComprobanteDto getComprobante() {
+        return comprobante;
+    }
+
+    public void setComprobante(ComprobanteDto comprobante) {
+        this.comprobante = comprobante;
+    }
+
+    public Date getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    public void setFechaModificacion(Date fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
+
+    public Integer getUsuarioModificacion() {
+        return usuarioModificacion;
+    }
+
+    public void setUsuarioModificacion(Integer usuarioModificacion) {
+        this.usuarioModificacion = usuarioModificacion;
+    }
+}
---
+++ Squirlearn/SoftMkpModel/src/main/java/pe/pucp/edu/prog3/softmkp/modelItem/ItemDto.java
@@ -0,0 +1,150 @@
+package pe.pucp.edu.prog3.softmkp.modelItem;
+
+    public class ItemDto {
+
+        private Integer itemId;
+        private Double precio;
+        private String nombre;
+        private Boolean esVenta;
+        private SubcategoriaDto subcategoria;
+        private CategoriaDto categoria;
+        private ColorDto color;
+        private FacultadDto facultad;
+        private EstadoItemDto estadoItem;
+        private CondicionDto condicion;
+        private TamanoDto tamano;
+        private FormatoDto formato;
+        
+        // Constructor vacío
+    public ItemDto() {
+        this.itemId = null;
+        this.precio = null;
+        this.nombre = null;
+        this.esVenta = null;
+        this.subcategoria = null;
+        this.categoria = null;
+        this.color = null;
+        this.facultad = null;
+        this.estadoItem = null;
+        this.condicion = null;
+        this.tamano = null;
+        this.formato = null;
+    }
+
+    // Constructor con parámetros
+    public ItemDto(Integer itemId, Double precio, String nombre, Boolean esVenta,
+                   SubcategoriaDto subcategoria, CategoriaDto categoria, ColorDto color,
+                   FacultadDto facultad, EstadoItemDto estadoItem, CondicionDto condicion,
+                   TamanoDto tamano, FormatoDto formato) {
+        this.itemId = itemId;
+        this.precio = precio;
+        this.nombre = nombre;
+        this.esVenta = esVenta;
+        this.subcategoria = subcategoria;
+        this.categoria = categoria;
+        this.color = color;
+        this.facultad = facultad;
+        this.estadoItem = estadoItem;
+        this.condicion = condicion;
+        this.tamano = tamano;
+        this.formato = formato;
+    }
+
+    // Getters y Setters
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getEsVenta() {
+        return esVenta;
+    }
+
+    public void setEsVenta(Boolean esVenta) {
+        this.esVenta = esVenta;
+    }
+
+    public SubcategoriaDto getSubcategoria() {
+        return subcategoria;
+    }
+
+    public void setSubcategoria(SubcategoriaDto subcategoria) {
+        this.subcategoria = subcategoria;
+    }
+
+    public CategoriaDto getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaDto categoria) {
+        this.categoria = categoria;
+    }
+
+    public ColorDto getColor() {
+        return color;
+    }
+
+    public void setColor(ColorDto color) {
+        this.color = color;
+    }
+
+    public FacultadDto getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(FacultadDto facultad) {
+        this.facultad = facultad;
+    }
+
+    public EstadoItemDto getEstadoItem() {
+        return estadoItem;
+    }
+
+    public void setEstadoItem(EstadoItemDto estadoItem) {
+        this.estadoItem = estadoItem;
+    }
+
+    public CondicionDto getCondicion() {
+        return condicion;
+    }
+
+    public void setCondicion(CondicionDto condicion) {
+        this.condicion = condicion;
+    }
+
+    public TamanoDto getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(TamanoDto tamano) {
+        this.tamano = tamano;
+    }
+
+    public FormatoDto getFormato() {
+        return formato;
+    }
+
+    public void setFormato(FormatoDto formato) {
+        this.formato = formato;
+    }
+
+}
