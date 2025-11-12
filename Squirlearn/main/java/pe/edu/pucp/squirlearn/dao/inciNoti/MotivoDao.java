e6ec9d30bdfb60a9f9370a10c86fd96905bed9ef
ArcX2001 <144306132+ArcX2001@users.noreply.github.com>
2025-11-11 20:13:48 -0500

    Delete Squirlearn/main/java/pe/edu/pucp/squirlearn directory

--- Squirlearn/main/java/pe/edu/pucp/squirlearn/dao/inciNoti/MotivoDao.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.pucp.squirlearn.dao.inciNoti;
-
-import pe.edu.pucp.squirlearn.model.inciNoti.MotivoDto;
-import pe.edu.pucp.squirlearn.dao.IDAO;
-
-public interface MotivoDao extends IDAO<MotivoDto>{
-    
-}
--- Squirlearn/main/java/pe/edu/pucp/squirlearn/dao/item/EstadoItemDao.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.pucp.squirlearn.dao.item;
-
-import pe.edu.pucp.squirlearn.model.item.EstadoItemDto;
-import pe.edu.pucp.squirlearn.dao.IDAO;
-
-public interface EstadoItemDao extends IDAO<EstadoItemDto>{
-    
-}
