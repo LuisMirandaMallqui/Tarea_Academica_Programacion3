f189574cffe1ebd784a216e0490b2dea6f7518ec
ArcX2001 <144306132+ArcX2001@users.noreply.github.com>
2025-11-16 08:30:32 -0500

    Delete Squirlearn/SquirlearnPersistance directory

--- Squirlearn/SquirlearnPersistance/src/main/java/pe/edu/pucp/squirlearn/dao/persona/EstadoPersonaDao.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.pucp.squirlearn.dao.persona;
-
-import pe.edu.pucp.squirlearn.model.persona.EstadoPersonaDto;
-import pe.edu.pucp.squirlearn.dao.IDAO;
-
-public interface EstadoPersonaDao extends IDAO<EstadoPersonaDto>{
-    
-}
--- Squirlearn/SquirlearnPersistance/src/main/java/pe/edu/pucp/squirlearn/daoImpl/util/Tipo_Operacion.java
+++
@@ -1,5 +0,0 @@
-package pe.edu.pucp.squirlearn.daoImpl.util;
-
-public enum Tipo_Operacion {
-    INSERTAR, MODIFICAR, ELIMINAR
-}
