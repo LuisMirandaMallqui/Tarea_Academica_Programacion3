572426130e729b39aa7bfb6ec827354d6f660cc5
LuisMirandaMallqui <a20223796@pucp.edu.pe>
2025-11-19 02:12:44 -0500

    wa
    wa

--- Base de Datos/Inserts_Iniciales.sql
+++ Base de Datos/Inserts_Iniciales.sql
@@ -63,7 +63,9 @@ INSERT INTO estados_publicaciones(NOMBRE) VALUES
 ('Pendiente'),
 ('Aprobada'),
 ('Rechazada'),
-('Eliminada');
+('Eliminada'),
+('Borrador'),
+('Transaccionada');
 INSERT INTO estados_chats(NOMBRE) VALUES 
 ('Activo'),
 ('Eliminado');
